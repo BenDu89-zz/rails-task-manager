f90bec0a1ae78a89cc71376548c9d96e6f50dc49
Benjamin Dunisch <bdunisch@googlemail.com>
2018-11-27 20:24:55 +0000

    added all r action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,15 +3,33 @@ class TasksController < ApplicationController
     @Tasks = Task.all
   end
   def new
+    @task = Task.new
   end
   def create
+    @task = Task.new(title: params[:task][:title], details: params[:task][:details])
+    @task.save
+    redirect_to tasks_path
   end
   def show
+    @Tasks = Task.all
+    @task = @Tasks.find(params[:id])
   end
   def edit
+    @task = Task.find(params[:id])
   end
   def update
+    @task_old = Task.find(params[:id])
+    @task_old.update(task_params)
   end
   def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
   end
+end
+
+private
+
+def task_params
+  params.require(:task).permit(:title, :details)
 end
